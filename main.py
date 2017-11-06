1f7bfd58ea4f6bfc64162619c18ff3b7e0a7819b
Babu Thomas <babuthomas138@gmail.com>
2017-11-07 03:35:35 +0530

    Get search results page html

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import csv
 import urllib.parse
+import urllib.request
+import time
+from bs4 import BeautifulSoup
 
 row_names = ['movie_id', 'movie_title']
 with open('u.item.txt', 'r', encoding = "ISO-8859-1") as f:
@@ -8,4 +11,8 @@ with open('u.item.txt', 'r', encoding = "ISO-8859-1") as f:
         movie_id = row['movie_id']
         movie_title = row['movie_title']
         url = 'http://www.imdb.com/find?q=' + urllib.parse.quote_plus(movie_title)
-        print(url)
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            soup = BeautifulSoup(html, 'html.parser')
+            print(soup.prettify())
+        time.sleep(1)

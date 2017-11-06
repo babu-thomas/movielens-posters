a2f95fb46d83089f13161001bbe0d8b88b6513dc
Babu Thomas <babuthomas138@gmail.com>
2017-11-07 03:15:28 +0530

    Make imdb search url

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import csv
+import urllib.parse
 
 row_names = ['movie_id', 'movie_title']
 with open('u.item.txt', 'r', encoding = "ISO-8859-1") as f:
     reader = csv.DictReader(f, fieldnames=row_names, delimiter='|')
     for row in reader:
-        print(row['movie_id'], row['movie_title'])
+        movie_id = row['movie_id']
+        movie_title = row['movie_title']
+        url = 'http://www.imdb.com/find?q=' + urllib.parse.quote_plus(movie_title)
+        print(url)
